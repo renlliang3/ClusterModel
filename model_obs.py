6565f93e842e0ae62203cf9161967ec4b26d6274
Remi Adam <adam.remi.gm@gmail.com>
2019-12-11 19:35:47 +0100

    apply Kcor to gamma and neutrinos

--- model_obs.py
+++ model_obs.py
@@ -75,7 +75,8 @@ class Observables(object):
     def get_gamma_spectrum(self, energy=np.logspace(-2,6,100)*u.GeV,
                            Rmin=None, Rmax=None,
                            type_integral='spherical',
-                           Rmin_los=None, NR500_los=5.0):
+                           Rmin_los=None, NR500_los=5.0,
+                           Kcor=True):
         """
         Compute the gamma ray emission enclosed within [Rmin,Rmax], in 3d (i.e. spherically 
         integrated), or the gamma ray emmission enclosed within an circular area (i.e.
@@ -91,6 +92,7 @@ class Observables(object):
         This is used only for cylindrical case
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
         This is used only for cylindrical case
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -102,6 +104,12 @@ class Observables(object):
         # In case the input is not an array
         energy = model_tools.check_qarray(energy, unit='GeV')
 
+        # K-correction
+        if Kcor:
+            energy_rf = energy*(1+self._redshift)
+        else:
+            energy_rf = energy*1.0
+        
         # Check the type of integral
         ok_list = ['spherical', 'cylindrical']
         if not type_integral in ok_list:
@@ -118,7 +126,7 @@ class Observables(object):
         # Compute the integral
         if type_integral == 'spherical':
             rad = model_tools.sampling_array(Rmin, Rmax, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_gamma_ray(energy, rad)
+            dN_dEdVdt = self.get_rate_gamma_ray(energy_rf, rad)
             dN_dEdt = model_tools.spherical_integration(dN_dEdVdt, rad)
             
         # Compute the integral        
@@ -128,7 +136,7 @@ class Observables(object):
             r3d = model_tools.sampling_array(Rmin3d*0.9, Rmax3d*1.1, NptPd=self._Npt_per_decade_integ, unit=True)
             los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
             r2d = model_tools.sampling_array(Rmin, Rmax, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_gamma_ray(energy, r3d)
+            dN_dEdVdt = self.get_rate_gamma_ray(energy_rf, r3d)
             dN_dEdt = model_tools.cylindrical_integration(dN_dEdVdt, energy, r3d, r2d, los, Rtrunc=self._R_truncation)
         
         # From intrinsic luminosity to flux
@@ -148,7 +156,8 @@ class Observables(object):
 
     def get_gamma_profile(self, radius=np.logspace(0,4,100)*u.kpc,
                           Emin=None, Emax=None, Energy_density=False,
-                          Rmin_los=None, NR500_los=5.0):
+                          Rmin_los=None, NR500_los=5.0,
+                          Kcor=True):
         """
         Compute the gamma ray emission profile within Emin-Emax.
         
@@ -161,6 +170,7 @@ class Observables(object):
         the number density is computed.
         - Rmin_los (quantity): minimal radius at which l.o.s integration starts
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -181,13 +191,19 @@ class Observables(object):
         Rmin = np.amin(radius.to_value('kpc'))*u.kpc
         Rmax = np.amax(radius.to_value('kpc'))*u.kpc
 
-        # Define array for integration
+        # Define energy and K correction
         eng = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
+        if Kcor:
+            eng_rf = eng*(1+self._redshift)
+        else:
+            eng_rf = eng*1.0
+
+        # Define array for integration
         Rmax3d = np.sqrt((NR500_los*self._R500)**2 + Rmax**2)        
         Rmin3d = np.sqrt(Rmin_los**2 + Rmin**2)
         r3d = model_tools.sampling_array(Rmin3d*0.9, Rmax3d*1.1, NptPd=self._Npt_per_decade_integ, unit=True)
         los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
-        dN_dEdVdt = self.get_rate_gamma_ray(eng, r3d)
+        dN_dEdVdt = self.get_rate_gamma_ray(eng_rf, r3d)
 
         # Apply EBL absorbtion
         if self._EBL_model != 'none':
@@ -223,7 +239,8 @@ class Observables(object):
     def get_gamma_flux(self, Emin=None, Emax=None, Energy_density=False,
                        Rmin=None, Rmax=None,
                        type_integral='spherical',
-                       Rmin_los=None, NR500_los=5.0):
+                       Rmin_los=None, NR500_los=5.0,
+                       Kcor=True):
         
         """
         Compute the gamma ray emission enclosed within Rmax, in 3d (i.e. spherically 
@@ -246,6 +263,7 @@ class Observables(object):
         This is used only for cylindrical case
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
         This is used only for cylindrical case
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -280,7 +298,7 @@ class Observables(object):
             # Get a spectrum
             energy = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
             energy, dN_dEdSdt = self.get_gamma_spectrum(energy, Rmin=Rmin, Rmax=Rmax,
-                                                        type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los)
+                                                        type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los, Kcor=Kcor)
 
             # Integrate over it and return
             flux = model_tools.energy_integration(dN_dEdSdt, energy, Energy_density=Energy_density)
@@ -290,7 +308,7 @@ class Observables(object):
             # Get a spectrum
             energy = model_tools.sampling_array(np.amin(Emin.value)*Emin.unit, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
             energy, dN_dEdSdt = self.get_gamma_spectrum(energy, Rmin=Rmin, Rmax=Rmax,
-                                                        type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los)
+                                                        type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los, Kcor=Kcor)
 
             # Integrate over it and return
             if Energy_density:
@@ -310,6 +328,11 @@ class Observables(object):
         if type(Rmax.value) == np.ndarray:
             # Get energy integration
             eng = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
+            if Kcor:
+                eng_rf = eng*(1+self._redshift)
+            else:
+                eng_rf = eng*1.0
+                
             if type_integral == 'spherical':
                 Rmax3d = np.amax(Rmax.value)*Rmax.unit
                 Rmin3d = Rmin
@@ -318,7 +341,7 @@ class Observables(object):
                 Rmin3d = np.sqrt(Rmin_los**2 + Rmin**2)*0.9
             r3d = model_tools.sampling_array(Rmin3d, Rmax3d, NptPd=self._Npt_per_decade_integ, unit=True)
             los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_gamma_ray(eng, r3d)
+            dN_dEdVdt = self.get_rate_gamma_ray(eng_rf, r3d)
 
             # Apply EBL absorbtion
             if self._EBL_model != 'none':
@@ -374,7 +397,8 @@ class Observables(object):
     def get_gamma_map(self, Emin=None, Emax=None,
                       Rmin_los=None, NR500_los=5.0,
                       Rmin=None, Rmax=None,
-                      Energy_density=False, Normalize=False):
+                      Energy_density=False, Normalize=False,
+                      Kcor=True):
         """
         Compute the gamma ray map. The map is normalized so that the integral 
         of the map over the cluster volume is 1 (up to Rmax=5R500).
@@ -395,6 +419,7 @@ class Observables(object):
         Has no effect if Normalized is True
         - Normalize (bool): if True, the map is normalized by the flux to get a 
         template in unit of sr-1 
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -422,7 +447,7 @@ class Observables(object):
         
         # Project the integrand
         r_proj, profile = self.get_gamma_profile(radius, Emin=Emin, Emax=Emax, Energy_density=Energy_density,
-                                                 Rmin_los=Rmin_los, NR500_los=NR500_los)
+                                                 Rmin_los=Rmin_los, NR500_los=NR500_los, Kcor=Kcor)
 
         # Convert to angle and interpolate onto a map
         theta_proj = (r_proj/self._D_ang).to_value('')*180.0/np.pi   # degrees
@@ -441,7 +466,7 @@ class Observables(object):
             if Rmin is None:
                 Rmin = self._Rmin
             flux = self.get_gamma_flux(Rmin=Rmin, Rmax=Rmax, type_integral='cylindrical', NR500_los=NR500_los,
-                                       Emin=Emin, Emax=Emax, Energy_density=Energy_density)
+                                       Emin=Emin, Emax=Emax, Energy_density=Energy_density, Kcor=Kcor)
             gamma_map = gamma_map / flux
             gamma_map = gamma_map.to('sr-1')
 
@@ -461,8 +486,9 @@ class Observables(object):
     def get_neutrino_spectrum(self, energy=np.logspace(-2,6,100)*u.GeV,
                               Rmin=None, Rmax=None,
                               type_integral='spherical',
-                              NR500_los=5.0,
-                              Rmin_los=None, flavor='all'):
+                              Rmin_los=None, NR500_los=5.0,
+                              flavor='all',
+                              Kcor=True):
         """
         Compute the neutrino emission enclosed within [Rmin,Rmax], in 3d (i.e. spherically 
         integrated), or the neutrino emmission enclosed within an circular area (i.e.
@@ -479,6 +505,7 @@ class Observables(object):
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
         This is used only for cylindrical case
         - flavor (str): either 'all', 'numu' or 'nue'
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -490,6 +517,12 @@ class Observables(object):
         # In case the input is not an array
         energy = model_tools.check_qarray(energy, unit='GeV')
 
+        # K-correction
+        if Kcor:
+            energy_rf = energy*(1+self._redshift)
+        else:
+            energy_rf = energy*1.0
+        
         # Check the type of integral
         ok_list = ['spherical', 'cylindrical']
         if not type_integral in ok_list:
@@ -506,7 +539,7 @@ class Observables(object):
         # Compute the integral
         if type_integral == 'spherical':
             rad = model_tools.sampling_array(Rmin, Rmax, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_neutrino(energy, rad, flavor=flavor)
+            dN_dEdVdt = self.get_rate_neutrino(energy_rf, rad, flavor=flavor)
             dN_dEdt = model_tools.spherical_integration(dN_dEdVdt, rad)
             
         # Compute the integral        
@@ -516,7 +549,7 @@ class Observables(object):
             r3d = model_tools.sampling_array(Rmin3d*0.9, Rmax3d*1.1, NptPd=self._Npt_per_decade_integ, unit=True)
             los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
             r2d = model_tools.sampling_array(Rmin, Rmax, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_neutrino(energy, r3d, flavor=flavor)
+            dN_dEdVdt = self.get_rate_neutrino(energy_rf, r3d, flavor=flavor)
             dN_dEdt = model_tools.cylindrical_integration(dN_dEdVdt, energy, r3d, r2d, los, Rtrunc=self._R_truncation)
         
         # From intrinsic luminosity to flux
@@ -531,7 +564,9 @@ class Observables(object):
 
     def get_neutrino_profile(self, radius=np.logspace(0,4,100)*u.kpc,
                              Emin=None, Emax=None, Energy_density=False,
-                             Rmin_los=None, NR500_los=5.0, flavor='all'):
+                             Rmin_los=None, NR500_los=5.0,
+                             flavor='all',
+                             Kcor=True):
         """
         Compute the neutrino emission profile within Emin-Emax.
         
@@ -545,6 +580,7 @@ class Observables(object):
         - Rmin_los (Quantity): the radius at which line of sight integration starts
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
         - flavor (str): either 'all', 'numu' or 'nue'
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -565,13 +601,19 @@ class Observables(object):
         Rmin = np.amin(radius.to_value('kpc'))*u.kpc
         Rmax = np.amax(radius.to_value('kpc'))*u.kpc
 
-        # Define array for integration
+        # Define energy and K correction
         eng = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
+        if Kcor:
+            eng_rf = eng*(1+self._redshift)
+        else:
+            eng_rf = eng*1.0
+        
+        # Define array for integration
         Rmax3d = np.sqrt((NR500_los*self._R500)**2 + Rmax**2)        
         Rmin3d = np.sqrt(Rmin_los**2 + Rmin**2)
         r3d = model_tools.sampling_array(Rmin3d*0.9, Rmax3d*1.1, NptPd=self._Npt_per_decade_integ, unit=True)
         los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
-        dN_dEdVdt = self.get_rate_neutrino(eng, r3d, flavor=flavor)
+        dN_dEdVdt = self.get_rate_neutrino(eng_rf, r3d, flavor=flavor)
 
         # Compute energy integal
         dN_dVdt = model_tools.energy_integration(dN_dEdVdt, eng, Energy_density=Energy_density)
@@ -603,7 +645,8 @@ class Observables(object):
                           Rmin=None, Rmax=None,
                           type_integral='spherical',
                           Rmin_los=None, NR500_los=5.0,
-                          flavor='all'):
+                          flavor='all',
+                          Kcor=True):
         
         """
         Compute the neutrino emission enclosed within Rmax, in 3d (i.e. spherically 
@@ -626,6 +669,7 @@ class Observables(object):
         This is used only for cylindrical case
         - NR500_los (float): the line-of-sight integration will stop at NR500_los x R500. 
         This is used only for cylindrical case
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -661,7 +705,7 @@ class Observables(object):
             energy = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
             energy, dN_dEdSdt = self.get_neutrino_spectrum(energy, Rmin=Rmin, Rmax=Rmax,
                                                            type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los,
-                                                           flavor=flavor)
+                                                           flavor=flavor, Kcor=Kcor)
 
             # Integrate over it and return
             flux = model_tools.energy_integration(dN_dEdSdt, energy, Energy_density=Energy_density)
@@ -672,7 +716,7 @@ class Observables(object):
             energy = model_tools.sampling_array(np.amin(Emin.value)*Emin.unit, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
             energy, dN_dEdSdt = self.get_neutrino_spectrum(energy, Rmin=Rmin, Rmax=Rmax,
                                                            type_integral=type_integral, Rmin_los=Rmin_los, NR500_los=NR500_los,
-                                                           flavor=flavor)
+                                                           flavor=flavor, Kcor=Kcor)
 
             # Integrate over it and return
             if Energy_density:
@@ -691,6 +735,11 @@ class Observables(object):
         if type(Rmax.value) == np.ndarray:
             # Get energy integration
             eng = model_tools.sampling_array(Emin, Emax, NptPd=self._Npt_per_decade_integ, unit=True)
+            if Kcor:
+                eng_rf = eng*(1+self._redshift)
+            else:
+                eng_rf = eng*1.0
+            
             if type_integral == 'spherical':
                 Rmax3d = np.amax(Rmax.value)*Rmax.unit
                 Rmin3d = Rmin
@@ -699,7 +748,7 @@ class Observables(object):
                 Rmin3d = np.sqrt(Rmin_los**2 + Rmin**2)*0.9
             r3d = model_tools.sampling_array(Rmin3d, Rmax3d, NptPd=self._Npt_per_decade_integ, unit=True)
             los = model_tools.sampling_array(Rmin_los, NR500_los*self._R500, NptPd=self._Npt_per_decade_integ, unit=True)
-            dN_dEdVdt = self.get_rate_neutrino(eng, r3d)
+            dN_dEdVdt = self.get_rate_neutrino(eng_rf, r3d)
 
             # Apply EBL absorbtion
             if self._EBL_model != 'none':
@@ -756,7 +805,8 @@ class Observables(object):
                          Rmin_los=None, NR500_los=5.0,
                          Rmin=None, Rmax=None,
                          Energy_density=False, Normalize=False,
-                         flavor='all'):
+                         flavor='all',
+                         Kcor=True):
         """
         Compute the neutrino map. The map is normalized so that the integral 
         of the map over the cluster volume is 1 (up to Rmax=5R500).
@@ -778,6 +828,7 @@ class Observables(object):
         - Normalize (bool): if True, the map is normalized by the flux to get a 
         template in unit of sr-1 
         - flavor (str): either 'all', 'numu' or 'nue'
+        - Kcor (bool): Apply the K correction (i.e. compute spectrum in rest frame energy)
 
         Outputs
         ----------
@@ -805,7 +856,7 @@ class Observables(object):
         
         # Project the integrand
         r_proj, profile = self.get_neutrino_profile(radius, Emin=Emin, Emax=Emax, Energy_density=Energy_density,
-                                                    Rmin_los=Rmin_los, NR500_los=NR500_los, flavor=flavor)
+                                                    Rmin_los=Rmin_los, NR500_los=NR500_los, flavor=flavor, Kcor=Kcor)
 
         # Convert to angle and interpolate onto a map
         theta_proj = (r_proj/self._D_ang).to_value('')*180.0/np.pi   # degrees
@@ -824,7 +875,7 @@ class Observables(object):
             if Rmin is None:
                 Rmin = self._Rmin
             flux = self.get_neutrino_flux(Rmin=Rmin, Rmax=Rmax, type_integral='cylindrical', NR500_los=NR500_los,
-                                          Emin=Emin, Emax=Emax, Energy_density=Energy_density, flavor=flavor)
+                                          Emin=Emin, Emax=Emax, Energy_density=Energy_density, flavor=flavor, Kcor=Kcor)
             nu_map = nu_map / flux
             nu_map = nu_map.to('sr-1')
 
