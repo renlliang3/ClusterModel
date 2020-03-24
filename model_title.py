8c70acb4f422deab1bd27a8c6e651b1d4ee52911
Remi Adam <adam.remi.gm@gmail.com>
2020-03-24 18:02:41 +0100

    change code name

--- model_title.py
+++ model_title.py
@@ -16,16 +16,6 @@ def show():
 
     """
 
-    print("=====================================================================")
-    print("                      ___   __    ___   __   __                      ")
-    print("                     / __) / _\  / __) /  \ (  )                     ")
-    print("                    ( (__ /    \( (_ \(  O )/ (_/\                   ")
-    print("                     \___)\_/\_/ \___/ \__/ \____/                   ")
-    print("=====================================================================")
-    print("   Cluster Atmosphere modeling for Gamma-ray Observations Libraries  ")
-    print("---------------------------------------------------------------------")
-    print("                                                                     ")
-
     #print("=================================================================")
     #print("                 ______ _____ _____ ______ _____                 ")
     #print("                 | ___ \  ___|  __ \| ___ \  ___|                ")
@@ -37,5 +27,80 @@ def show():
     #print("  Pipeline for the Estimation of Gamma Ray Emission in clusters  ")
     #print("-----------------------------------------------------------------")
     #print("                                                                 ")
+    
+    #print("==================================================================")
+    #print("                      ___   __    ___   __   __                   ")
+    #print("                     / __) / _\  / __) /  \ (  )                  ")
+    #print("                    ( (__ /    \( (_ \(  O )/ (_/\                ")
+    #print("                     \___)\_/\_/ \___/ \__/ \____/                ")
+    #print("==================================================================")
+    #print(" Cluster Atmosphere modeling for Gamma-ray Observations Libraries ")
+    #print("------------------------------------------------------------------")
+    #print("                                                                  ")
+    
+    #print("==================================================================")
+    #print("              ___ ___  __  __  ___ ___    _ _____ ___             ")
+    #print("             / __/ _ \|  \/  |/ __| _ \  /_\_   _| __|            ")
+    #print("            | (_| (_) | |\/| | (__|   / / _ \| | | _|             ")
+    #print("             \___\___/|_|  |_|\___|_|_\/_/ \_\_| |___|            ")
+    #print("==================================================================")
+    #print(" Cluster Observables Modeling for Cosmic Ray And Thermal Emission ")
+    #print("------------------------------------------------------------------")
+    #print("                                                                  ")
 
-    # Galaxy Cluster Hot Gas Modeling Pipeline Gamma Ray Observations Analysis and Multi-Wavelength
+    print("================================================================")
+    print("                 __  __ ___ ___   _   ___   ___                 ")
+    print("                |  \/  |_ _/ __| /_\ |   \ / _ \                ")
+    print("                | |\/| || | (__ / _ \| |) | (_) |               ")
+    print("                |_|  |_|___\___/_/ \_\___/ \___/                ")   
+    print("================================================================")
+    print(" Modeling the Intra Cluster Atmosphere for Diverse Observations ")
+    print("----------------------------------------------------------------")
+    print("                                                                ")
+
+    """
+    Galaxy
+    Cluster
+    Intra
+    Diffuse
+    Emission
+    Atmosphere 
+    Modeling 
+    Observations 
+    Software 
+    Tools
+    Pipeline
+    Estimation
+    Gamma
+    Ray
+    Cosmic
+
+    Intra Cluster Medium Thermal and Nonthermal Modeling Multiwavelenght Observation
+    ICMTNMMO
+
+    Thermal And Non Thermal Intra Cluster Medium Modeling Software for Multi Wavelenght Observation
+
+    TANTICMMSMWO
+
+    Thermal And Nonthermal Emission Software for Modeling Galaxy Cluster 
+    TANESMGC
+    
+    Software Intra Cluster Thermal And Non Thermal Emission Modeling
+    SICTANEM
+
+    Cluster Atmosphere Diffuse Emission Modeling
+    CADEM
+
+    Cluster Atmosphere Modeling Multiwavelenght Observations
+    Cluster Atmosphere Diffuse Emission Modeling Software For Multiwavelength Observations
+    CADEMSO
+    CATANM
+
+    Cluster ICM Emission Tool                -> CIEMT
+    Cluster ICM Emission Modeling Algorithm  -> CIEMA
+    Cluster ICM Atmosphere Non-thermal Model -> CIANM
+
+    Intra Cluster Gas and Cosmic Ray Model   -> ICGCRM
+
+    Cosmic Ray And Thermal Emission Modeling for Clusters Observables -> CRATEMC
+    """
